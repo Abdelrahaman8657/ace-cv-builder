58a6bc01a96c34dd57aa2ef7e4b5e9a899d2be80
Abdelrahaman8657 <body132547698@gmail.com>
2026-01-02 13:40:44 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Portfolio/", // ضيف السطر ده باسم المستودع بتاعك بين علامتين /
   server: {
     host: "::",
     port: 8080,
